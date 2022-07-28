a24bb2cf817b07620d7cd5d36f9726f4327a3aa7
Julien Bidoret <julien@accentygrave.net>
2022-07-28 18:57:35 +0200

    Font-changer buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -145,6 +145,28 @@ readmoreclose.forEach( (link) => {
 })
 
 
+var fontChangers = document.querySelectorAll(".changefont");
+if(fontChangers){
+  fontChangers.forEach((changer) => {
+    changer.onclick = () => {
+      createCookie("font", changer.dataset.font, 15);
+      document.location.reload();
+    }
+  })
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/; SameSite=Lax";
+}
+
+
+
 // Faire, défaire et refaire, c’est toujours travailler pour rien :D
 // var ajaxRequest = new (function () {
 
