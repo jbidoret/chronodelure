9b54d83fdb45ab7012050e9c4c202e20fa27bb52
Julien Bidoret <julien@accentygrave.net>
2022-08-12 12:15:43 +0200

    Replaced dash by comma in prev/next links

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -1,10 +1,10 @@
 <nav class="prevnext">
   <div class="max">
     <?php if ($page->hasPrevListed($sessions)): $prev = $page->prevListed($sessions);?>
-      <a class="prevnext-prev" href="<?= $prev->url() ?>">← <span class="prevnext-year"><?= $prev->year() ?></span> <span class="prevnext-dash"> —</span> <span class="prevnext-title"><?= $prev->title() ?></span></a>
+      <a class="prevnext-prev" href="<?= $prev->url() ?>">← <span class="prevnext-year"><?= $prev->year() ?></span><span class="prevnext-dash">, </span> <span class="prevnext-title"><?= $prev->title() ?></span></a>
     <?php endif ?>
     <?php if ($page->hasNextListed($sessions)): $next = $page->nextListed($sessions);?>
-      <a class="prevnext-next" href="<?= $next->url() ?>"><span class="prevnext-year"><?= $next->year() ?></span> <span class="prevnext-dash"> —</span> <span class="prevnext-title"><?= $next->title() ?></span> →</a>
+      <a class="prevnext-next" href="<?= $next->url() ?>"><span class="prevnext-year"><?= $next->year() ?></span><span class="prevnext-dash">, </span> <span class="prevnext-title"><?= $next->title() ?></span> →</a>
     <?php endif ?>
   </div>
 </nav>
