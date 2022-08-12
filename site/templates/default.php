b4e85ced71b9650a4e537baa21d39bcb3b4e342f
Julien Bidoret <julien@accentygrave.net>
2022-08-12 12:16:17 +0200

    Font changer buttons

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,6 +6,15 @@
       </div>
     </header>
     <article class="default-article">
+      <div class="max fontchanger">
+        <p>À l’occasion des 70<sup>e</sup> Rencontres internationales de Lure, la discussion fera rage autour du choix typographique de la Timelure. Cliquez sur les boutons ci-dessous, et rejoignez le débat&nbsp;:</p>
+        <p class="buttons">
+          <button class="changefont" id="changefont-source" data-font="Source Serif VF">Source Serif</button>
+          <button class="changefont" id="changefont-fern" data-font="Fern Web">Fern</button>
+          <button class="changefont" id="changefont-roboto" data-font="Roboto Serif">Roboto Serif</button>
+        </p>
+        
+      </div>
       <div class="max">
         <?= $page->text()->kt() ?>
       </div>
